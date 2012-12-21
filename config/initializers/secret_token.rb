d2e8a9259990eb04bc71ab53754712c3f871ad7d
Tatsunori TAMAKI <github@qtamaki.com>
2012-12-21 11:12:32 +0900

    init rails 3 project with MySQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoldRush::Application.config.secret_token = 'de37935fadd39ac14990096da1a7ff8bfaccd72a76cbea03845077548a4d3d9875342cf862ae8f0568ed4b0fb4bf779eda99e57da5fa5e58ef5769a6c5a9018c'
