da0e9495ae3c9c214b09249d4e3ba7a2ee52b183
keiteio <mss.ymns@gmail.com>
2013-04-05 14:14:51 +0900

    httpclient実行スクリプトの修正

--- tools/httpclient.sh
+++ tools/httpclient.sh
@@ -1,14 +1,15 @@
 #!/bin/sh -f
-POSTFIX_ENV_FILE=$1
 
-. $POSTFIX_ENV_FILE
-RAILS_ROOT=$1
+# 第一引数：環境設定ファイル
+#  以下の変数が定義されている事
+#    RAILS_ROOT  : railsアプリケーションのルートディレクトリへの絶対パス
+#    RUBY        : rubyの実行ファイルへの絶対パス
+. $1
 shift 1
-RUBY=/usr/bin/ruby
-#echo $1 $2 $3
+
+# 第二引数以降：Rubyスクリプトのパラメータ
 
 umask 002
 
-cd $RAILS_ROOT
-$RUBY bin/httpclient.rb $*
-
+cd ${RAILS_ROOT}
+${RUBY} tools/httpclients.rb $*
